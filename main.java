4717a9525c7ba44060cbd548b35372cf33c24c94
vvvaaa2 <137019399+vvvaaa2@users.noreply.github.com>
2023-06-20 15:51:40 +0800

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
-package xxx
+package main1
 
 import cn.com.csge
 import cn.com.csgmall.*
 import cn.csg
 
-//南方电网互联网服务有限公司、南网互联网、赫兹助手源码、赫兹乐购、test
-//测试xxxxxx
+//南方电网互联网服务有限公司、南网互联网、赫兹助手源码、赫兹乐购
+//password123456 
